e3790d3e413baf4ad2c42a63914eb015260f4de5
larkstaden <larkstaden@gmail.com>
2021-01-28 22:35:22 +0100

    sed fixed. Banners hidden

--- publish-script.sh
+++ publish-script.sh
@@ -4,15 +4,15 @@
  git stash --quiet
  git pull --rebase --quiet
  git stash pop --quiet
- #sed -i '/<a href="https:\/\/mobirise.site\/v" style=/d' docs/*.html
+ sed -i '' -e "s/display: flex/display: none/g" docs/*.html
  git add docs
  git commit docs -m "Commit by publish-script.sh on $(date)"
  git push
  echo ""
  echo " ________________________________________________________ " 
  echo "| Script is done :-)                                     |"
- #echo "| In addition to syncing with GitHub, it removes all the |"
- #echo "| annoying mobirise banners.                             |"
+ echo "| In addition to syncing with GitHub, it hides all the   |"
+ echo "| annoying mobirise banners by setting 'display: none'   |"
  echo "| Changes should be on Github and the website in seconds |"
  echo "|                                                        |"
  echo "| Press Enter to exit...                                 |"
