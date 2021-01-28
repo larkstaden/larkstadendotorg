23d0446637fc6e9bdd25a9334b4ed9223f756bfd
larkstaden <larkstaden@gmail.com>
2021-01-28 16:52:33 +0100

    fixed error in publish script

--- publish-script.sh
+++ publish-script.sh
@@ -4,16 +4,15 @@
  git stash --quiet
  git pull --rebase --quiet
  git stash pop --quiet
- sed -i "/mobirise/d" docs/*.html
+ sed -i "/mobirise.site/d" docs/*.html
  git add docs
  git commit docs -m "Commit by publish-script.sh on $(date)"
  git push
  echo ""
  echo " ________________________________________________________ " 
  echo "| Script is done :-)                                     |"
- echo "| In addition to syncing with GitHub, it removes all     |"
- echo "| lines containing the word mobirise from all html files |"
- echo "| removing the annoying mobirise banners.                |"
+ echo "| In addition to syncing with GitHub, it removes all the |"
+ echo "| annoying mobirise banners.                             |"
  echo "| Changes should be on Github and the website in seconds |"
  echo "|                                                        |"
  echo "| Press Enter to exit...                                 |"
