2443a62915c8ef4c7ed08999446c619abd3fd4b0
larkstaden <larkstaden@gmail.com>
2021-01-28 13:26:50 +0100

    spaces before each line to ommit from command history

--- initial-setup-script.sh
+++ initial-setup-script.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-git config user.name larkstaden
-git config user.email larkstaden@gmail.com
-git config --global core.safecrlf false
-echo ""
-echo " ________________________________________________________ " 
-echo "| initial-setup-script is done configuring git on this pc|"
-echo "|                                                        |"
-echo "| Set username to larkstaden                             |"
-echo "| Set email to larkstaden@gmail.com                      |"
-echo "| Enable CLRF line ending conversion without warnings    |"
-echo "|                                                        |"
-echo "| Press Enter to exit...                                 |"
-echo " ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ "
-read
+ git config user.name larkstaden
+ git config user.email larkstaden@gmail.com
+ git config --global core.safecrlf false
+ echo ""
+ echo " ________________________________________________________ " 
+ echo "| initial-setup-script is done configuring git on this pc|"
+ echo "|                                                        |"
+ echo "| Set username to larkstaden                             |"
+ echo "| Set email to larkstaden@gmail.com                      |"
+ echo "| Enable CLRF line ending conversion without warnings    |"
+ echo "|                                                        |"
+ echo "| Press Enter to exit...                                 |"
+ echo " ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ "
+ read
 
--- publish-script.sh
+++ publish-script.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-#git checkout master
-git add docs
-git commit docs -m "Added by publish-script.sh on $(date)"
-git push
-echo ""
-echo " ________________________________________________________ " 
-echo "| Script is done :-)                                     |"
-echo "|                                                        |"
-echo "| Changes should be on Github and the website in seconds |"
-echo "| Want to double check?                                  |"
-echo "| Visit: https://github.com/larkstaden/larkstadendotorg  |" 
-echo "|                                                        |"
-echo "| Press Enter to exit...                                 |"
-echo " ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ "
-read
+ #git checkout master
+ git add docs
+ git commit docs -m "Added by publish-script.sh on $(date)"
+ git push
+ echo ""
+ echo " ________________________________________________________ " 
+ echo "| Script is done :-)                                     |"
+ echo "|                                                        |"
+ echo "| Changes should be on Github and the website in seconds |"
+ echo "| Want to double check?                                  |"
+ echo "| Visit: https://github.com/larkstaden/larkstadendotorg  |" 
+ echo "|                                                        |"
+ echo "| Press Enter to exit...                                 |"
+ echo " ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ "
+ read
 
